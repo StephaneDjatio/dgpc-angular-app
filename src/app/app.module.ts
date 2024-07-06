25b442c418cc7b48e36698550ed207abbb8ef7ab
pro <stephanedjatio@gmail.com>
2024-07-06 03:39:27 +0100

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicComponent } from './public/public.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PublicComponent
+    PublicComponent,
+    DocumentationComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
