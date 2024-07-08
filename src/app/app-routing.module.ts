49110a7e83402341b229c63bec0371db55e87052
pro <stephanedjatio@gmail.com>
2024-07-08 13:49:32 +0100

    main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public/public.component';
+import { DocumentationComponent } from './documentation/documentation.component';
 
 const routes: Routes = [
   {path:'public', component: PublicComponent},
+  {path:'documentation', component: DocumentationComponent},
   { path:'', redirectTo:'public', pathMatch:'full'},
   // Other top-level routes
   // { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
